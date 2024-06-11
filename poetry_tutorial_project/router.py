d2dc723c45bc1ecf79366bc59573f2a44f03c2b9
hivinpolat <hivinpolat_3823@hotmail.com>
2024-06-12 01:17:31 +0300

    dbfh

--- poetry_tutorial_project/router.py
+++ poetry_tutorial_project/router.py
@@ -3,6 +3,7 @@ from leanapi.path import controller
 @controller("personnel", router)
 class Personnel:
 
+
     @router.get("/get/{personnel_id}")
     def get_personel(self, personnel_id: int) -> dict:
         return {"personnel_id": personnel_id}
